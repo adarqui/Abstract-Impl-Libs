28844ac93c173cd51c00b67a04f1dd65335db040
Andrew Darqui <andrew.darqui@gmail.com>
2014-08-28 23:04:44 -0400

    counters

--- src/Abstract/Impl/Libs/Counter.hs
+++ src/Abstract/Impl/Libs/Counter.hs
@@ -1,12 +1,14 @@
 module Abstract.Impl.Libs.Counter (
  module Abstract.Interfaces.Counter,
  mkCounter'MVar,
-
  mkCounter'MVar'Inc,
  mkCounter'MVar'Dec,
  mkCounter'MVar'Get,
 
- mkCounter'IORef
+ mkCounter'IORef,
+ mkCounter'IORef'Inc,
+ mkCounter'IORef'Dec,
+ mkCounter'IORef'Get
 ) where
 
 import Abstract.Interfaces.Counter
@@ -17,3 +19,6 @@ import Abstract.Impl.Libs.Counter.MVar.Dec (mkCounter'MVar'Dec)
 import Abstract.Impl.Libs.Counter.MVar.Get (mkCounter'MVar'Get)
 
 import Abstract.Impl.Libs.Counter.IORef.Internal (mkCounter'IORef)
+import Abstract.Impl.Libs.Counter.IORef.Inc (mkCounter'IORef'Inc)
+import Abstract.Impl.Libs.Counter.IORef.Dec (mkCounter'IORef'Dec)
+import Abstract.Impl.Libs.Counter.IORef.Get (mkCounter'IORef'Get)
